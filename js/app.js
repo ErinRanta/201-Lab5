235a256f53f86c3b50fff02a4adac9fd47e315a8
Erin Ranta <erinran99@gmail.com>
2022-06-10 19:40:21 -0400

    not done

--- js/app.js
+++ js/app.js
@@ -8,9 +8,9 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(4, 7) { //eslint-disable-line
+// function sum(4, 7) { //eslint-disable-line
 
-return[11];
+// return[11];
  
 
 // Here is the test for sum(); uncomment it to run it
@@ -27,12 +27,12 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function testmMultiply(a,b) { //eslint-disable-line
 
-}
+}let resultMultiply = (multiply4*5)
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -68,7 +68,7 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
+let  = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
