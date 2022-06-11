dbcdeb2016d91e54605f4de32aaffe51bbd0f61d
Erin Ranta <erinran99@gmail.com>
2022-06-11 18:53:58 -0400

    2ndprob

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
+// Write your code heree
 function multiply(a,b) {//eslint-disable-line
   let returnMul = a * b;
   let concatstring = 'The product of ${a} and ${b} is ${returnMul}.';
