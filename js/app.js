f0dc085d17383aabc09a5c4abeb5d8621808003c
Erin Ranta <erinran99@gmail.com>
2022-06-11 22:37:33 -0400

    prob4

--- js/app.js
+++ js/app.js
@@ -68,20 +68,23 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-let  = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+let firstEl = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2]);
+let secondEl = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${firstEl[0]} is their sum.`;
+let returnArray = [firstEl[0], secondEl];
+return returnArray;
 
-} er 
+}
+Here is the test for sumArray(); uncomment it to run it
 
-// Here is the test for sumArray(); uncomment it to run it
-
-// testSumArray(testArray);
+testSumArray(testArray); 
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
-/* Problem 5
+/* Problem 5 
 Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The numbers 2,3,4 have a product of 24."
