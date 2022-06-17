95c1877c430f35ad42229de8f1ce2f476f3b7847
Erin Ranta <erinran99@gmail.com>
2022-06-17 11:39:10 -0400

    deleted the myterious r

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
-"The sum of 4 and 7 is 11." r
+"The sum of 4 and 7 is 11." 
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
@@ -80,13 +80,7 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 
-// function sumArray(sumArr) { //eslint-disable-line
-// let firstEl = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2]);
-// let secondEl = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${firstEl[0]} is their sum.`;
-// let returnArray = [firstEl[0], secondEl];
-// return returnArray;
 
-}
 // Here is the test for sumArray(); uncomment it to run it
 
 // testSumArray(testArray); 
